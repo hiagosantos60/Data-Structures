68cc3417cacf6930f051101739f9c8d5f4f6a12f
Hiago Santos <hiagorsantos60@gmail.com>
2024-05-23 20:04:05 -0300

    .

--- Arrays/reverse_a_string.py
+++ Arrays/reverse_a_string.py
@@ -1,1 +1,15 @@
+linha = 'eu sou o hiago'
 
+def reverse(linha):
+   
+    palavras = linha.split()[::-1]
+
+    reverse_palavras = [letra[::-1] for letra in palavras]
+
+    reverse_linha = ' '.join(reverse_palavras)
+
+    return reverse_linha
+
+linha_reverse = reverse(linha)
+
+print(linha_reverse)
