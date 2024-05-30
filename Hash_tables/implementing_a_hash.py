8b51349d1f1531798a9d669097e727cd7f7ad9c5
Hiago Santos <hiagorsantos60@gmail.com>
2024-05-30 16:49:05 -0300

    .

---
+++ Hash_tables/implementing_a_hash.py
@@ -0,0 +1,6 @@
+#no python, uma hash table é um dicionario {} basicamente. É um O(1), ou seja, tem lookups, insetion e deletes muito rápidos.
+
+class HashTable():
+    def __init__(self, size):
+        size.self = size
+        self.data = [None]*self.data #Aqui inicializamos com o array em zero
