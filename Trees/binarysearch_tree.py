01d52b00d2a70d4bc98e1edaed565098ddeb0fbd
Hiago Santos <hiagorsantos60@gmail.com>
2024-07-24 15:47:42 -0300

    .

---
+++ Trees/binarysearch_tree.py
@@ -0,0 +1,13 @@
+
+#Primeiro vou iniciar minha estrutura de árvore vazia
+class Node():
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+#aqui inicamos o construtor da estrutura com o init sendo o nó que dará inicio a minha árvore
+class search():
+    def __inti__(self):
+        self.root = None
+        self.numero_de_nos = 0
